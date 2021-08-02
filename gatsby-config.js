ce3c8f413a118499db3a7c48800c4b2835a749b0
qinzhongyi <qinzhongyi@deepexi.com>
2021-08-02 18:47:24 +0800

    try change .mdx file to .md

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     `gatsby-plugin-netlify`,
   ],
 };
