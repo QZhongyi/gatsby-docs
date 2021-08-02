d693206cb9defc3052a2b38b89094a472a8027e6
qinzhongyi <qinzhongyi@deepexi.com>
2021-08-02 18:04:51 +0800

    feat: start up

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Frontend Docs`,
+    defaultTitle: `Frontend Docs`,
+    siteTitleShort: `Frontend Docs`,
+    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
+    siteUrl: `https://rocketdocs.netlify.app`,
+    siteAuthor: `@qinzhongyi`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
+  },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        repositoryUrl: `https://github.com/QZhongyi/gatsby-docs`,
+        baseDir: ``,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.app`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`,
+  ],
+};
